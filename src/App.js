8f3ff683b4edc3b9d67b60c6a1483d36ac4b8570
ArthurYaxiongWangRRC <yaxiongwang@rrc.ca>
2024-08-29 15:35:55 -0500

    Done with the count component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>test</div>;
+  const [count, setCount] = useState(0);
+
+  function Count({ num }) {
+    return <div>Count: {num}</div>;
+  }
+
+  function handleDeduction() {
+    setCount((count) => count - 1);
+  }
+
+  function handleAddition() {
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <>
+      <button onClick={handleDeduction}>-</button>
+      <Count num={count} />
+      <button onClick={handleAddition}>+</button>
+    </>
+  );
 }
