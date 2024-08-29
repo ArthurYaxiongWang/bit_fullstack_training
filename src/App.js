7b8b69e6b1c9b24dd48233bbc0f2fa3804d9dfa6
ArthurYaxiongWangRRC <yaxiongwang@rrc.ca>
2024-08-29 15:43:53 -0500

    Done with Step component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [stepCount, setStepCount] = useState(0);
+
+  function Step({ num }) {
+    function handleDeduction() {
+      setStepCount((stepCount) => stepCount - 1);
+    }
+
+    function handleAddition() {
+      setStepCount((stepCount) => stepCount + 1);
+    }
+
+    return (
+      <div>
+        <button onClick={handleDeduction}>-</button>
+        Step: {stepCount}
+        <button onClick={handleAddition}>+</button>
+      </div>
+    );
+  }
 
   function Count({ num }) {
-    return <div>Count: {num}</div>;
-  }
+    function handleDeduction() {
+      setCount((count) => count - 1);
+    }
 
-  function handleDeduction() {
-    setCount((count) => count - 1);
-  }
+    function handleAddition() {
+      setCount((count) => count + 1);
+    }
 
-  function handleAddition() {
-    setCount((count) => count + 1);
+    return (
+      <div>
+        <button onClick={handleDeduction}>-</button>
+        Count: {num}
+        <button onClick={handleAddition}>+</button>
+      </div>
+    );
   }
 
   return (
     <>
-      <button onClick={handleDeduction}>-</button>
+      <Step />
       <Count num={count} />
-      <button onClick={handleAddition}>+</button>
     </>
   );
 }
