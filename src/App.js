fe73a7a22e752e11e05a16d759a0b2892a5888d0
ArthurYaxiongWangRRC <yaxiongwang@rrc.ca>
2024-08-29 21:08:58 -0500

    Done with the practice project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   const [count, setCount] = useState(0);
   const [stepCount, setStepCount] = useState(0);
 
-  function Step({ num }) {
+  function Step() {
     function handleDeduction() {
       setStepCount((stepCount) => stepCount - 1);
     }
@@ -22,20 +22,36 @@ export default function App() {
     );
   }
 
-  function Count({ num }) {
+  function Count() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + count);
+
     function handleDeduction() {
-      setCount((count) => count - 1);
+      setCount((count) => count - stepCount);
     }
 
     function handleAddition() {
-      setCount((count) => count + 1);
+      setCount((count) => count + stepCount);
+    }
+
+    let message;
+    if (count === 0) {
+      message = "Today is ";
+    } else if (count > 0) {
+      message = `${count} days from today is `;
+    } else {
+      message = `${Math.abs(count)} days ago was `;
     }
 
     return (
       <div>
         <button onClick={handleDeduction}>-</button>
-        Count: {num}
+        Count: {count}
         <button onClick={handleAddition}>+</button>
+        <p>
+          <span>{message}</span>
+          <span>{date.toDateString()}</span>
+        </p>
       </div>
     );
   }
@@ -43,7 +59,7 @@ export default function App() {
   return (
     <>
       <Step />
-      <Count num={count} />
+      <Count />
     </>
   );
 }
